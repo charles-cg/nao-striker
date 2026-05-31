84c1e990cd95cb48edf1f1387b3de39774db7a97
charles-cg <carlitoscarreong@hotmail.com>
2026-05-31 16:00:37 -0600

    modified gitignore and added the run test file

---
+++ run_mock.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, division
+
+from striker.context import Context
+from striker import state_machine
+from vision import mock
+
+ctx = Context(vision=mock, motion=None, tts=None, logger=print)
+state_machine.run(ctx)
